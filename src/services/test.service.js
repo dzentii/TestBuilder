75948463e70a720516b0682dfdbd5aa5fb2237d8
dzentii <rzhnik@gmail.com>
2025-06-18 13:54:11 +0500

    fix

--- src/services/test.service.js
+++ src/services/test.service.js
@@ -2,32 +2,32 @@ import { toast } from "react-toastify"
 import { API } from "./api"
 
 const createTest = async(data) => {
-  const response = await API.post("/tests", data)
+  const response = await API.post("/api/tests", data)
   return response.data
 }
 
 export const getTests = async() => {
-  const response = await API.get("/tests")
+  const response = await API.get("/api/tests")
   return response.data
 }
 
 export const getTest = async(testId) => {
-  const response = await API.get("/tests/" + testId)
+  const response = await API.get("/api/tests/" + testId)
   return response.data
 }
 
 export const deleteTest = async(testId) => {
-  const response = await API.delete("/tests/" + testId)
+  const response = await API.delete("/api/tests/" + testId)
   return response.data
 }
 
 export const createQuestionsToTest = async (testId, questions) => {
-  const response = await API.post("/questions/test/" + testId, questions)
+  const response = await API.post("/api/questions/test/" + testId, questions)
   return response.data
 }
 
 export const startTest = async (testId, guestInfo) => {
-  const response = await API.post("/results/start", {
+  const response = await API.post("/api/results/start", {
     testId, guestInfo
   })
 
@@ -37,7 +37,7 @@ export const startTest = async (testId, guestInfo) => {
 export const submitTest = async (testId, data) => {
   try {
 
-    const response = await API.post(`/results/${testId}/submit`, data)
+    const response = await API.post(`/api/results/${testId}/submit`, data)
     return 200
   } catch (error) {
     // console.log(error)
@@ -46,23 +46,23 @@ export const submitTest = async (testId, data) => {
 }
 
 export const getAllTestResults = async (testId) => {
-  const response = await API.get("/results/test/" + testId)
+  const response = await API.get("/api/results/test/" + testId)
   return response.data
 }
 
 export const getManualTest = async(resultId) => {
-  const response = await API.get(`/results/${resultId}`);
+  const response = await API.get(`/api/results/${resultId}`);
   const data = response.data;
   return data
 }
 
 export const checkManualTest = async (resultId, data) => {
-  const response = await API.post(`/results/${resultId}/review`, {answers: data})
+  const response = await API.post(`/api/results/${resultId}/review`, {answers: data})
   return response.data
 }
 
 export const updateTest = async (testId, data)  => {
-  const response = await API.put("/tests/" + testId, data)
+  const response = await API.put("/api/tests/" + testId, data)
   return response
 }
 
